48c678acf8aef4bb1ca91e59083491ab452d3669
CryptoCanary <ssaxton@kent.edu>
2018-10-31 07:20:50 -0400

    Default .pid sterlingcoinD.

--- src/util.cpp
+++ src/util.cpp
@@ -540,7 +540,7 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
 #ifndef WIN32
 boost::filesystem::path GetPidFile()
 {
-    boost::filesystem::path pathPidFile(GetArg("-pid", "sterlingcoin.pid"));
+    boost::filesystem::path pathPidFile(GetArg("-pid", "sterlingcoind.pid"));
     if (!pathPidFile.is_complete()) pathPidFile = GetDataDir() / pathPidFile;
     return pathPidFile;
 }
