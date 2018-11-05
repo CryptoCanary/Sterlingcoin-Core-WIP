62b2c9850cddf0fd969db1cfbbf9918ad5f83be6
CryptoCanary <ssaxton@kent.edu>
2018-11-04 22:43:52 -0500

    url

--- src/config/bitcoin-config.h
+++ src/config/bitcoin-config.h
@@ -284,22 +284,22 @@
 #define LT_OBJDIR ".libs/"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "www.sterlingcoin.org"
+#define PACKAGE_BUGREPORT "Sterlingcoin.org"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "Sterlinogcoin Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Sterlinogcoin Core 0.1.0"
+#define PACKAGE_STRING "Sterlinogcoin Core 0.14.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "sterlingcoin"
 
 /* Define to the home page for this package. */
-#define PACKAGE_URL ""
+#define PACKAGE_URL "Sterlingcoin.org"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.1.0"
+#define PACKAGE_VERSION "0.14.1"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
