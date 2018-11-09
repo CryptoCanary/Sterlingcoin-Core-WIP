d5ebc73f13066d15ef1b918acb8079be8c96f720
CryptoCanary <ssaxton@kent.edu>
2018-11-08 22:25:43 -0500

    Add spinner image 35.

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -55,7 +55,7 @@ static const int MAX_URI_LENGTH = 255;
 #define EXPORT_IMAGE_SIZE 256
 
 /* Number of frames in spinner animation */
-#define SPINNER_FRAMES 35
+#define SPINNER_FRAMES 36
 
 #define QAPP_ORG_NAME "Sterlingcoin"
 #define QAPP_ORG_DOMAIN "Sterlingcoin.org"
