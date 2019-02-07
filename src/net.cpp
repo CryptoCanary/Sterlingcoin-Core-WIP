16853c3c53187b5ee880eb63f1bd9bf29510d925
Steven Saxton <ssaxton@kent.edu>
2019-02-07 17:29:59 -0500

    Restarts staking after exception

--- src/net.cpp
+++ src/net.cpp
@@ -3,7 +3,7 @@
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2015-2018 The PIVX developers
 // Copyright (c) 2018 The Helium developers
-// Copyright (c) 2014-2018 The Sterlingcoin developers
+// Copyright (c) 2014-2019 The Sterlingcoin developers
 
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
@@ -1569,6 +1569,7 @@ void static ThreadStakeMinter()
         boost::this_thread::interruption_point();
     } catch (std::exception& e) {
         LogPrintf("ThreadStakeMinter() exception \n");
+	    ThreadStakeMinter();
     } catch (...) {
         LogPrintf("ThreadStakeMinter() error \n");
     }
